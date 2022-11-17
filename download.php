b75211b66d80a19ebd3739635168b5e624a30c4a
Ignas <statkus.online@gmail.com>
2022-11-17 19:14:55 +0200

    download function added

--- download.php
+++ download.php
@@ -1,15 +1,16 @@
 <?php
-
-$file = $_POST['filepath'];
-
-if (file_exists($file)) {
-    header('Content-Type: application/actet-stream');
-    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-    header('Content-Length:' . filesize($file));
-    readfile($file);
-    die;
+if (isset($_POST['filepath'])) {
+    $file = $_POST['filepath'];
+    if (file_exists($file)) {
+        header('Content-Type: application/actet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+        header('Content-Length:' . filesize($file));
+        readfile($file);
+        die;
+    } else {
+        header('Location: /paskaitos/forms/16p_pvz/index.php?message=File does not exist.');
+    }
 } else {
-    header('Location: /paskaitos/forms/16p_pvz/index.php?message=File does not exist.');
+    header('Location: /paskaitos/forms/16p_pvz/index.php?message=Filename is not defined.');
 }
-
 
